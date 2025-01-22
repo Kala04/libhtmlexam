54ec9a7dbad119a250b8c84c24c262e96c5685ba
Kala04 <153944588+Kala04@users.noreply.github.com>
2025-01-22 14:54:34 +0530

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,18 @@
+import { createConnection } from 'mysql';
+
+const connection = createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'bookStore'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL database: ', err);
+        return;
+    }
+    console.log('Connected to MySQL database.');
+});
+
+export default connection;
